fe6aa4c01e1fd65912dfeefa78d2ced984ade5fa
toriqpriad <toriq.354@gmail.com>
2017-06-04 10:49:34 +0700

    test

---
+++ application/views/admin/include/sidebar_menu.php
@@ -0,0 +1,41 @@
+<body>
+
+    <div class="wrapper">
+        <div class="sidebar" data-background-color="white" data-active-color="danger">
+            <div class="sidebar-wrapper">
+                <div class="logo">
+                    <a href="http://www.creative-tim.com" class="simple-text">
+                        Yasarkonv
+                    </a>
+                </div>
+
+                <ul class="nav">
+                    <?php
+                    if (isset($menu)) {
+                        foreach ($menu as $data) {
+                            if ($active_page == $data['page_name']) {
+                                ?>
+                                <li class="active">
+                                    <a href="<?= $data['link'] ?>">
+                                        <i class="<?= $data['icon'] ?>"></i>
+                                        <p><?= $data['label'] ?></p>
+                                    </a>
+                                </li>
+                                <?php
+                            } else {
+                                ?>
+                                <li>
+                                    <a href="<?= $data['link'] ?>">
+                                        <i class="<?= $data['icon'] ?>"></i>
+                                        <p><?= $data['label'] ?></p>
+                                    </a>
+                                </li>
+                                <?php
+                            }
+                        }
+                    }
+                    ?>
+
+                </ul>
+            </div>
+        </div>
